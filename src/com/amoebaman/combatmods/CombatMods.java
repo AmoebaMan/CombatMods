781df32f90b44c5b84d69932417b67af4cc7be5b
Dennison Richter <denny.ty@comcast.net>
2013-01-28 17:49:38 -0500

    Add ballistae, add text macros for messages

--- src/com/amoebaman/combatmods/CombatMods.java
+++ src/com/amoebaman/combatmods/CombatMods.java
@@ -1,12 +1,18 @@
 package com.amoebaman.combatmods;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Effect;
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.ConfigurationSection;
@@ -30,9 +36,11 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.vehicle.VehicleDestroyEvent;
 import org.bukkit.event.vehicle.VehicleExitEvent;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.PluginLogger;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.Vector;
 
 import com.amoebaman.pvpstattracker.StatHandler;
 import com.amoebaman.pvpstattracker.Statistic;
@@ -40,9 +48,10 @@ import com.amoebaman.pvpstattracker.Statistic;
 public class CombatMods extends JavaPlugin implements Listener{
 
 	private PluginLogger log;
-	private ConfigurationSection parrying, headshots, lunging, armoredBoats, fastArrows, arrowRetrieval, antispamBows, brokenKnees, assassinations;
+	private ConfigurationSection parrying, headshots, lunging, armoredBoats, fastArrows, arrowRetrieval, antispamBows, brokenKnees, assassinations, ballistae;
 	private File configFile;
 	private boolean statTracking;
+	private HashSet<Byte> transparent;
 	
 	public void onEnable(){
 		log = new PluginLogger(this);
@@ -51,8 +60,19 @@ public class CombatMods extends JavaPlugin implements Listener{
 		try{ loadConfig(); }
 		catch(Exception e){ e.printStackTrace(); }
 		Bukkit.getPluginManager().registerEvents(this, this);
+		transparent = new HashSet<Byte>(); transparent.add((byte) 0); transparent.add((byte) 6); transparent.add((byte) 8);
+		transparent.add((byte) 9); transparent.add((byte) 20); transparent.add((byte) 27); transparent.add((byte) 28);
+		transparent.add((byte) 31); transparent.add((byte) 32); transparent.add((byte) 37); transparent.add((byte) 38);
+		transparent.add((byte) 39); transparent.add((byte) 40); transparent.add((byte) 50); transparent.add((byte) 55);
+		transparent.add((byte) 63); transparent.add((byte) 65); transparent.add((byte) 66); transparent.add((byte) 68);
+		transparent.add((byte) 69); transparent.add((byte) 70); transparent.add((byte) 72); transparent.add((byte) 75);
+		transparent.add((byte) 76); transparent.add((byte) 77); transparent.add((byte) 78); transparent.add((byte) 93);
+		transparent.add((byte) 94); transparent.add((byte) 96); transparent.add((byte) 101); transparent.add((byte) 102);
+		transparent.add((byte) 104); transparent.add((byte) 105); transparent.add((byte) 106); transparent.add((byte) 111);
+		transparent.add((byte) 131); transparent.add((byte) 132); transparent.add((byte) 143); transparent.add((byte) 23);
+		transparent.add((byte) 126);
 	}
-	
+
 	public void loadConfig() throws Exception{
 		try{
 			if(!configFile.exists()){
@@ -70,6 +90,7 @@ public class CombatMods extends JavaPlugin implements Listener{
 			antispamBows = getConfig().getConfigurationSection("antispam-bows");
 			brokenKnees = getConfig().getConfigurationSection("broken-knees");
 			assassinations = getConfig().getConfigurationSection("assassinations");
+			ballistae = getConfig().getConfigurationSection("ballistae");
 		}
 		catch(Exception e){
 			e.printStackTrace();
@@ -84,7 +105,7 @@ public class CombatMods extends JavaPlugin implements Listener{
 		}
 		
 		for(String component : getConfig().getKeys(false))
-			log.fine(component + " " + (getConfig().getConfigurationSection(component).getBoolean("enabled") ? "enabled" : "disabled"));
+			log.info(component + " " + (getConfig().getConfigurationSection(component).getBoolean("enabled") ? "enabled" : "disabled"));
 	}
 
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
@@ -101,7 +122,7 @@ public class CombatMods extends JavaPlugin implements Listener{
 		return true;
 	}
 	
-	private HashMap<String, Long> lastParryAttempt = new HashMap<String, Long>();
+	private static final HashMap<String, Long> lastParryAttempt = new HashMap<String, Long>();
 	@EventHandler
 	public void detectParrying(PlayerInteractEvent event){
 		if(!parrying.getBoolean("enabled"))
@@ -109,16 +130,16 @@ public class CombatMods extends JavaPlugin implements Listener{
 		final Player player = event.getPlayer();
 		if(player.getItemInHand() == null)
 			return;
-		Material inHand = player.getItemInHand().getType();
-		if(inHand.name().contains("SWORD") && event.getAction().name().contains("RIGHT_CLICK")){
+		final ItemStack inHand = player.getItemInHand();
+		if(inHand.getType().name().contains("SWORD") && event.getAction().name().contains("RIGHT_CLICK")){
 			if(!lastParryAttempt.containsKey(player.getName()))
 				lastParryAttempt.put(player.getName(), 0L);
 			if(System.currentTimeMillis() - lastParryAttempt.get(player.getName()) < parrying.getInt("fumble-time") && Math.random() < parrying.getDouble("fumble-chance"))
 				Bukkit.getScheduler().scheduleSyncDelayedTask(this, new Runnable(){ public void run(){
-					player.getWorld().dropItemNaturally(player.getLocation(), player.getItemInHand());
-					player.getInventory().remove(player.getItemInHand());
-					player.sendMessage(ChatColor.translateAlternateColorCodes('&', parrying.getString("fumble-message")));
-				}});
+				player.getWorld().dropItemNaturally(player.getLocation(), inHand).setPickupDelay(20);
+				player.getInventory().remove(inHand);
+				player.sendMessage(ChatColor.translateAlternateColorCodes('&', parrying.getString("fumble-message")));
+			}});
 			else
 				lastParryAttempt.put(player.getName(), System.currentTimeMillis());
 		}
@@ -130,7 +151,7 @@ public class CombatMods extends JavaPlugin implements Listener{
 			return;
 		if(!(event.getEntity() instanceof Player))
 			return;
-		Player player = (Player) event.getEntity();
+		final Player player = (Player) event.getEntity();
 		if(player.isBlocking() && player.getNoDamageTicks() == 0 && (event.getCause() == DamageCause.ENTITY_ATTACK || event.getCause() == DamageCause.PROJECTILE)){
 			if(!lastParryAttempt.containsKey(player.getName()))
 				lastParryAttempt.put(player.getName(), 0L);
@@ -141,11 +162,12 @@ public class CombatMods extends JavaPlugin implements Listener{
 				if(event.getDamage() > player.getHealth() && statTracking)
 					StatHandler.incrementStat(player, "life-saving parries");
 			}
-			else if(System.currentTimeMillis() - lastParryAttempt.get(player.getName()) > parrying.getInt("disarm-time") && Math.random() < parrying.getDouble("disarm-chance")){
-				player.getWorld().dropItemNaturally(player.getLocation(), player.getItemInHand());
-				player.getInventory().remove(player.getItemInHand());
-				player.sendMessage(ChatColor.translateAlternateColorCodes('&', parrying.getString("disarm-message")));
-			}
+			else if(System.currentTimeMillis() - lastParryAttempt.get(player.getName()) > parrying.getInt("disarm-time") && Math.random() < parrying.getDouble("disarm-chance"))
+				Bukkit.getScheduler().scheduleSyncDelayedTask(this, new Runnable(){ public void run(){
+					player.getWorld().dropItemNaturally(player.getLocation(), player.getItemInHand());
+					player.getInventory().remove(player.getItemInHand());
+					player.sendMessage(ChatColor.translateAlternateColorCodes('&', parrying.getString("disarm-message")));
+				}});
 		}
 	}
 	
@@ -164,8 +186,8 @@ public class CombatMods extends JavaPlugin implements Listener{
 			if(proj.getLocation().distance(player.getEyeLocation()) <= 1.2){
 				event.setDamage((int) (event.getDamage() * headshots.getDouble("multiplier")));
 				if(proj.getShooter() instanceof Player)
-					((Player) proj.getShooter()).sendMessage(ChatColor.translateAlternateColorCodes('&', headshots.getString("dealt-message")));
-				player.sendMessage(ChatColor.translateAlternateColorCodes('&', headshots.getString("taken-message")));
+					((Player) proj.getShooter()).sendMessage(ChatColor.translateAlternateColorCodes('&', headshots.getString("dealt-message").replace("%victim%", player.getName())));
+				player.sendMessage(ChatColor.translateAlternateColorCodes('&', headshots.getString("taken-message").replace("%player%", (((Player) proj.getShooter()).getName()))));
 				if(statTracking)
 					StatHandler.incrementStat((Player) proj.getShooter(), "headshots"); 
 			}
@@ -224,11 +246,12 @@ public class CombatMods extends JavaPlugin implements Listener{
 		if(!(event instanceof EntityDamageByEntityEvent))
 			return;
 		EntityDamageByEntityEvent eEvent = (EntityDamageByEntityEvent) event;
-		if(eEvent.getDamager() instanceof Arrow)
-			event.setDamage((int) (event.getDamage() / fastArrows.getDouble("speed"))); 
+		if(eEvent.getDamager() instanceof Arrow && !ballistaArrows.contains(eEvent.getDamager().getEntityId()))
+			event.setDamage((int) (event.getDamage() / fastArrows.getDouble("speed")));
+		ballistaArrows.remove(eEvent.getDamager().getUniqueId());
 	}
 	
-	private HashMap<LivingEntity, Integer> arrowsLodged = new HashMap<LivingEntity, Integer>();
+	private static final HashMap<LivingEntity, Integer> arrowsLodged = new HashMap<LivingEntity, Integer>();
 	@EventHandler
 	public void arrowRetrievalLogger(EntityDamageEvent event){
 		if(!arrowRetrieval.getBoolean("enabled"))
@@ -294,7 +317,7 @@ public class CombatMods extends JavaPlugin implements Listener{
 			shooter.sendMessage(ChatColor.translateAlternateColorCodes('&', antispamBows.getString("message")));
 		}
 	}
-	
+
 	@EventHandler
 	public void brokenKnees(EntityDamageEvent event){
 		if(!brokenKnees.getBoolean("enabled"))
@@ -324,18 +347,19 @@ public class CombatMods extends JavaPlugin implements Listener{
 			if(testEvent.isCancelled())
 				return;
 			Bukkit.getScheduler().scheduleSyncDelayedTask(this, new Runnable(){ public void run(){
-				if(victim.getHealth() > 0 && player.getLocation().distance(victim.getLocation()) < 2 && (Math.abs(getYaw(player) - getYaw(victim)) < 45 || Math.abs(getYaw(player) - getYaw(victim)) > 315)){
+				if(victim.getHealth() > 0 && player.getHealth() > 0 && player.getLocation().distance(victim.getLocation()) < 2 && (Math.abs(getYaw(player) - getYaw(victim)) < 45 || Math.abs(getYaw(player) - getYaw(victim)) > 315))
+					victim.sendMessage(ChatColor.translateAlternateColorCodes('&', assassinations.getString("warning-message").replace("%player%", player.getName())));
+			}}, 20 * assassinations.getInt("warning-time", 500) / 1000);
+			Bukkit.getScheduler().scheduleSyncDelayedTask(this, new Runnable(){ public void run(){
+				if(victim.getHealth() > 0 && player.getHealth() > 0 && player.getLocation().distance(victim.getLocation()) < 2 && (Math.abs(getYaw(player) - getYaw(victim)) < 45 || Math.abs(getYaw(player) - getYaw(victim)) > 315)){
 					victim.setHealth(0);
-					player.sendMessage(ChatColor.translateAlternateColorCodes('&', assassinations.getString("dealt-message")));
-					victim.sendMessage(ChatColor.translateAlternateColorCodes('&', assassinations.getString("taken-message")));
+					victim.setLastDamageCause(new EntityDamageEvent(victim, DamageCause.CUSTOM, 0));
+					player.sendMessage(ChatColor.translateAlternateColorCodes('&', assassinations.getString("dealt-message").replace("%victim%", victim.getName())));
+					victim.sendMessage(ChatColor.translateAlternateColorCodes('&', assassinations.getString("taken-message").replace("%player%", player.getName())));
 					if(statTracking)
 						StatHandler.incrementStat(player, "assassinations");
 				}
-			}}, 20);
-			Bukkit.getScheduler().scheduleSyncDelayedTask(this, new Runnable(){ public void run(){
-				if(victim.getHealth() == 0)
-					victim.setLastDamageCause(testEvent);
-			}}, 20);
+			}}, 20 * assassinations.getInt("death-time", 500) / 1000);
 		}
 	}
 	
@@ -348,4 +372,86 @@ public class CombatMods extends JavaPlugin implements Listener{
 		return yaw;
 	}
 	
+	private static final float ARROW_SPEED = 3;
+	private static final HashMap<String, Long> launchTimes = new HashMap<String, Long>();
+	private static final ArrayList<Integer> ballistaArrows = new ArrayList<Integer>(); 
+	@EventHandler
+	public void ballistae(PlayerInteractEvent event){
+		if(!ballistae.getBoolean("enabled"))
+			return;
+		Player player = event.getPlayer();
+		if(event.getAction().name().contains("LEFT_CLICK")){
+			Vector dir = player.getLocation().getDirection();
+			BlockFace face;
+			if(Math.abs(dir.getX()) >= Math.abs(dir.getZ())){
+				if(dir.getX() >= 0)
+					face = BlockFace.EAST;
+				else
+					face = BlockFace.WEST;
+			}
+			else{
+				if(dir.getZ() >= 0)
+					face = BlockFace.SOUTH;
+				else
+					face = BlockFace.NORTH;
+			}
+			Block center = player.getLocation().getBlock();
+			
+			Block ballista = getBallista(center, face);
+					
+			if(ballista != null){
+				if(!launchTimes.containsKey(player.getName()))
+					launchTimes.put(player.getName(), System.currentTimeMillis());
+				if(System.currentTimeMillis() - launchTimes.get(player.getName()) <= ballistae.getInt("cooldown")){
+					player.sendMessage(ChatColor.translateAlternateColorCodes('&', ballistae.getString("cooldown-message")));
+					return;
+				}
+				Location projSpawn = ballista.getLocation().clone().add(0.5, 0.5, 0.5);
+				List<Block> line = player.getLineOfSight(transparent, 250);
+				Location target = line.get(line.size() - 1).getLocation();
+				Vector velocity = target.clone().subtract(projSpawn.clone()).toVector();
+				velocity = velocity.multiply(1 / velocity.length());
+				
+				Inventory inv = player.getInventory();
+				Material munitions = Material.AIR;
+				if(inv.contains(Material.ARROW))
+					munitions = Material.ARROW;
+				switch(munitions){
+				case ARROW:
+					Arrow arrow = player.getWorld().spawnArrow(projSpawn, velocity, (float)(ARROW_SPEED * ballistae.getDouble("arrow-flight-speed")), 0);
+					arrow.setShooter(player);
+					ballistaArrows.add(arrow.getEntityId());
+					player.getWorld().playEffect(projSpawn, Effect.BOW_FIRE, 0);
+					launchTimes.put(player.getName(), System.currentTimeMillis());
+					break;
+				default:
+					player.sendMessage(ChatColor.translateAlternateColorCodes('&', ballistae.getString("no-ammo-message")));
+				}
+				int slot = inv.first(munitions);
+				if(slot >= 0){
+					ItemStack ammo = inv.getItem(slot);
+					ammo.setAmount(ammo.getAmount() - 1);
+					inv.setItem(slot, ammo);
+				}
+			}
+		}
+	}
+	
+	public boolean isBallista(Block center, BlockFace face){
+		BlockFace a = BlockFace.values()[(face.ordinal() == 0) ? 3 : face.ordinal() - 1];
+		BlockFace b = BlockFace.values()[(face.ordinal() == 3) ? 0 : face.ordinal() + 1];
+		Block left = center.getRelative(a);
+		Block right = center.getRelative(b);
+		return center.getType() == Material.DISPENSER && left.getType() == Material.WOOD_STEP && left.getData() >= 8 && right.getType() == Material.WOOD_STEP && right.getData() >= 8;
+	}
+	
+	public Block getBallista(Block center, BlockFace face){
+		if(isBallista(center.getRelative(face), face))
+			return center.getRelative(face).getRelative(face);
+		if(isBallista(center.getRelative(BlockFace.DOWN), face))
+			return center.getRelative(BlockFace.DOWN).getRelative(face);
+		if(isBallista(center.getRelative(BlockFace.DOWN).getRelative(face), face))
+			return center.getRelative(BlockFace.DOWN).getRelative(face).getRelative(face);
+		return null;
+	}
 }
