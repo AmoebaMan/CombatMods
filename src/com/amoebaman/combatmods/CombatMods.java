2a82727308e3fc10e8bf493a5001b7891d1db0a5
Dennison Richter <drichter@amoebaman.net>
2014-02-26 14:50:05 -0500

    Fix wtftext and compress imports

--- src/com/amoebaman/combatmods/CombatMods.java
+++ src/com/amoebaman/combatmods/CombatMods.java
@@ -1,63 +1,30 @@
 package com.amoebaman.combatmods;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 import net.amoebaman.statmaster.StatMaster;
 import net.amoebaman.statmaster.Statistic;
 
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.Effect;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.block.Block;
-import org.bukkit.block.BlockFace;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
+import org.bukkit.*;
+import org.bukkit.command.*;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.enchantments.Enchantment;
-import org.bukkit.entity.Arrow;
-import org.bukkit.entity.Boat;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.LivingEntity;
-import org.bukkit.entity.Player;
-import org.bukkit.entity.Projectile;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
-import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.entity.*;
+import org.bukkit.event.*;
+import org.bukkit.event.entity.*;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
-import org.bukkit.event.entity.EntityDeathEvent;
-import org.bukkit.event.entity.PlayerDeathEvent;
-import org.bukkit.event.entity.ProjectileLaunchEvent;
-import org.bukkit.event.player.PlayerInteractEntityEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.event.player.PlayerMoveEvent;
-import org.bukkit.event.vehicle.VehicleDestroyEvent;
-import org.bukkit.event.vehicle.VehicleExitEvent;
-import org.bukkit.inventory.Inventory;
+import org.bukkit.event.player.*;
+import org.bukkit.event.vehicle.*;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.metadata.FixedMetadataValue;
 import org.bukkit.plugin.PluginLogger;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.util.Vector;
 
 public class CombatMods extends JavaPlugin implements Listener{
-
-	private PluginLogger log;
-<<<<<<< HEAD
 	
-=======
-	private ConfigurationSection parrying, headshots, lunging, armoredBoats, fastArrows, arrowRetrieval, antispamBows, brokenKnees, assassinations, ballistae, noDurability;
->>>>>>> 1d6199e47d0d6e089444a93e5978d9600bd0217f
+	private PluginLogger log;
 	private File configFile;
 	private ConfigurationSection parrying, headshots, lunging, armoredBoats, fastArrows, arrowRetrieval, antispamBows, brokenKnees, assassinations, noDurability;
-	
 	private boolean statTracking;
 	private HashSet<Byte> transparent;
 	
@@ -80,7 +47,7 @@ public class CombatMods extends JavaPlugin implements Listener{
 		transparent.add((byte) 131); transparent.add((byte) 132); transparent.add((byte) 143); transparent.add((byte) 23);
 		transparent.add((byte) 126);
 	}
-
+	
 	public void loadConfig() throws Exception{
 		try{
 			if(!configFile.exists()){
@@ -98,10 +65,6 @@ public class CombatMods extends JavaPlugin implements Listener{
 			antispamBows = getConfig().getConfigurationSection("antispam-bows");
 			brokenKnees = getConfig().getConfigurationSection("broken-knees");
 			assassinations = getConfig().getConfigurationSection("assassinations");
-<<<<<<< HEAD
-=======
-			ballistae = getConfig().getConfigurationSection("ballistae");
->>>>>>> 1d6199e47d0d6e089444a93e5978d9600bd0217f
 			noDurability = getConfig().getConfigurationSection("no-durability");
 		}
 		catch(Exception e){
@@ -119,7 +82,7 @@ public class CombatMods extends JavaPlugin implements Listener{
 		for(String component : getConfig().getKeys(false))
 			log.info(component + " " + (getConfig().getConfigurationSection(component).getBoolean("enabled") ? "enabled" : "disabled"));
 	}
-
+	
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
 		if(command.getName().equals("cmreload")){
 			try{
@@ -148,10 +111,10 @@ public class CombatMods extends JavaPlugin implements Listener{
 				lastParryAttempt.put(player.getName(), 0L);
 			if(System.currentTimeMillis() - lastParryAttempt.get(player.getName()) < parrying.getInt("fumble-time") && Math.random() < parrying.getDouble("fumble-chance"))
 				Bukkit.getScheduler().scheduleSyncDelayedTask(this, new Runnable(){ public void run(){
-				player.getWorld().dropItemNaturally(player.getLocation(), inHand).setPickupDelay(20);
-				player.getInventory().remove(inHand);
-				player.sendMessage(ChatColor.translateAlternateColorCodes('&', parrying.getString("fumble-message")));
-			}});
+					player.getWorld().dropItemNaturally(player.getLocation(), inHand).setPickupDelay(20);
+					player.getInventory().remove(inHand);
+					player.sendMessage(ChatColor.translateAlternateColorCodes('&', parrying.getString("fumble-message")));
+				}});
 			else
 				lastParryAttempt.put(player.getName(), System.currentTimeMillis());
 		}
@@ -296,7 +259,7 @@ public class CombatMods extends JavaPlugin implements Listener{
 			victim.getWorld().dropItemNaturally(victim.getLocation(), new ItemStack(Material.ARROW, arrowsLodged.get(victim)));
 		arrowsLodged.remove(victim);
 	}
-
+	
 	private HashMap<String, Long> lastBowDraw = new HashMap<String, Long>();
 	@EventHandler
 	public void detectBowDraw(PlayerInteractEvent event){
@@ -328,7 +291,7 @@ public class CombatMods extends JavaPlugin implements Listener{
 			shooter.sendMessage(ChatColor.translateAlternateColorCodes('&', antispamBows.getString("message")));
 		}
 	}
-
+	
 	@EventHandler
 	public void brokenKnees(EntityDamageEvent event){
 		if(!brokenKnees.getBoolean("enabled"))
@@ -382,104 +345,9 @@ public class CombatMods extends JavaPlugin implements Listener{
 			yaw += 360;
 		return yaw;
 	}
-
-	@SuppressWarnings("deprecation")
-	@EventHandler
-	public void infiniteWeaponArmorDurability(EntityDamageEvent event){
-		if(!noDurability.getBoolean("enabled"))
-			return;
-		if(event.getEntityType() == EntityType.PLAYER){
-			final Player victim = (Player) event.getEntity();
-			for(final ItemStack armor : victim.getInventory().getArmorContents())
-				if(armor != null)
-					Bukkit.getScheduler().scheduleSyncDelayedTask(this, new Runnable(){ public void run(){
-						armor.setDurability((short) 0);
-						victim.updateInventory();
-					}});
-		}
-		if(event instanceof EntityDamageByEntityEvent){
-			EntityDamageByEntityEvent eEvent = (EntityDamageByEntityEvent) event;
-			final Player damager;
-			if(eEvent.getDamager().getType() == EntityType.PLAYER)
-				damager = (Player) eEvent.getDamager();
-			else if(eEvent.getDamager().getType() == EntityType.ARROW){
-				Arrow arrow = (Arrow) eEvent.getDamager();
-				if(arrow.getShooter().getType() == EntityType.PLAYER)
-					damager = (Player) arrow.getShooter();
-				else
-					damager = null;
-			}
-<<<<<<< HEAD
-			else
-				damager = null;
-			if(damager != null){
-				Material weapon = damager.getItemInHand().getType();
-				if(weapon.name().contains("SWORD") || weapon.name().contains("AXE"))
-					Bukkit.getScheduler().scheduleSyncDelayedTask(this, new Runnable(){ public void run(){
-						damager.getItemInHand().setDurability((short) 0);
-						damager.updateInventory();
-					}});
-=======
-			Block center = player.getLocation().getBlock();
-			
-			Block ballista = getBallista(center, face);
-					
-			if(ballista != null){
-				if(!launchTimes.containsKey(player.getName()))
-					launchTimes.put(player.getName(), 0L);
-				if(System.currentTimeMillis() - launchTimes.get(player.getName()) <= ballistae.getInt("cooldown")){
-					player.sendMessage(ChatColor.translateAlternateColorCodes('&', ballistae.getString("cooldown-message")));
-					return;
-				}
-				Location projSpawn = ballista.getLocation().clone().add(0.5, 0.5, 0.5);
-				List<Block> line = player.getLineOfSight(transparent, 250);
-				Location target = line.get(line.size() - 1).getLocation();
-				Vector velocity = target.clone().subtract(projSpawn.clone()).toVector();
-				velocity = velocity.multiply(1 / velocity.length());
-				
-				Inventory inv = player.getInventory();
-				Material munitions = Material.AIR;
-				if(inv.contains(Material.ARROW))
-					munitions = Material.ARROW;
-				switch(munitions){
-				case ARROW:
-					Arrow arrow = player.getWorld().spawnArrow(projSpawn, velocity, (float)(ARROW_SPEED * ballistae.getDouble("arrow-flight-speed")), 0);
-					arrow.setShooter(player);
-					ballistaArrows.add(arrow.getEntityId());
-					player.getWorld().playEffect(projSpawn, Effect.BOW_FIRE, 0);
-					launchTimes.put(player.getName(), System.currentTimeMillis());
-					break;
-				default:
-					player.sendMessage(ChatColor.translateAlternateColorCodes('&', ballistae.getString("no-ammo-message")));
-				}
-				int slot = inv.first(munitions);
-				if(slot >= 0){
-					ItemStack ammo = inv.getItem(slot);
-					ammo.setAmount(ammo.getAmount() - 1);
-					inv.setItem(slot, ammo);
-				}
->>>>>>> 1d6199e47d0d6e089444a93e5978d9600bd0217f
-			}
-		}
-	}
 	
 	@SuppressWarnings("deprecation")
 	@EventHandler
-	public void infiniteBowDurability(ProjectileLaunchEvent event){
-		if(!noDurability.getBoolean("enabled"))
-			return;
-		if(event.getEntityType() == EntityType.ARROW){
-			final Arrow arrow = (Arrow) event.getEntity();
-			if(arrow.getShooter() != null && arrow.getShooter().getType() == EntityType.PLAYER)
-				Bukkit.getScheduler().scheduleSyncDelayedTask(this, new Runnable(){ public void run(){
-					((Player) arrow.getShooter()).getItemInHand().setDurability((short) 0);
-					((Player) arrow.getShooter()).updateInventory();
-				}});
-		}
-	}
-
-	@SuppressWarnings("deprecation")
-	@EventHandler
 	public void infiniteWeaponArmorDurability(EntityDamageEvent event){
 		if(!noDurability.getBoolean("enabled"))
 			return;
@@ -513,6 +381,7 @@ public class CombatMods extends JavaPlugin implements Listener{
 						damager.getItemInHand().setDurability((short) 0);
 						damager.updateInventory();
 					}});
+				
 			}
 		}
 	}
